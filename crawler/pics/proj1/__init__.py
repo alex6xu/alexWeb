45612b3e77a9b52e80153306c936b0bd9fac6a7e
alex <xuhui@weiyankeji.cn>
2019-07-26 14:40:51 +0800

    spider 1

---
+++ crawler/pics/proj1/__init__.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+sqlite_conn = sqlite3.connect('../../../image.db')
+
+try:
+    sqlite_conn.execute('''create table if not exists image(
+      ID INTEGER PRIMARY KEY AUTOINCREMENT,
+      url text,
+      title text,
+      desd text
+    )
+    ''')
+    sqlite_conn.commit()
+
+except Exception as e:
+    print(e)
+
+
+def get_conn():
+    return sqlite_conn
+
+
+def commit():
+    sqlite_conn.commit()
+
+
+def close():
+    sqlite_conn.close()
--- crawler/pics/proj1/spiders/doutula.py
+++ crawler/pics/proj1/spiders/doutula.py
@@ -3,6 +3,7 @@ import scrapy
 from bs4 import BeautifulSoup
 # import js2xml
 # from lxml import etree
+from proj1 import get_conn, commit, close
 
 
 class Doutula(scrapy.Spider):
@@ -11,6 +12,15 @@ class Doutula(scrapy.Spider):
     base_url = 'http://www.doutula.com/'
 
     url = 'http://www.doutula.com/index.php'
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate, br',
+               'Accept-Language': 'zh-CN,zh;q=0.9',
+               'pragma': 'no-cache',
+               'Connection': 'keep-alive',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+               }
+
+    conn = get_conn()
 
     def start_requests(self):
         yield scrapy.Request(url=self.url, callback=self.parse, method='GET', headers=self.headers, dont_filter=True)
@@ -22,5 +32,18 @@ class Doutula(scrapy.Spider):
         if content is not None:
             for ct in content:
                 image = ct.find('img', attrs={"referrerpolicy": "no-referrer"})
-                image_url = image.src
-                print(image_url)
+                image_url = image['data-original']
+                self.save_pic(image_url)
+
+        commit()
+        close()
+
+    def save_pic(self, imageurl):
+
+        sql = """
+            insert into image (url, title, desd) 
+            values ('%s', '%s', '%s')
+        """ % (imageurl, imageurl, imageurl)
+        print(sql)
+        self.conn.execute(sql)
+
